3861a2d611716609a90e74dc6d0342dc881959a5
Ruben Fefer <rubenx4p@gmail.com>
2020-03-14 11:37:57 +0200

    refactor auth.js

--- src/api/routes/auth.js
+++ src/api/routes/auth.js
@@ -6,11 +6,11 @@ router.post('/', async (req, res, next) => {
     try {
         const user = await User.findOne({ email: req.body.email })
         if (!user) {
-            return res.status(400).send("Invalid email or password")
+            return res.status(400).send({ msg: "Invalid email or password"})
         }
         const validPassword = await bcrypt.compare(req.body.password, user.password)
         if (!validPassword) {
-            return res.status(400).send("Invalid email or password")
+            return res.status(400).send({ msg: "Invalid email or password"})
         }
         const token = user.generateAuthToken();
         return res.status(200).json({
@@ -21,7 +21,7 @@ router.post('/', async (req, res, next) => {
     catch(err) {
             console.log(err);
             result.status(500).json({
-                error: err
+                msg: err
             })
     }
 });
