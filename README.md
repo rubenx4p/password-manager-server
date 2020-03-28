6f30d77d0d76918e4d1e1691ce933e9b2fce0da8
Ruben Fefer <rubenx4p@gmail.com>
2020-03-28 23:47:55 +0300

    add env variables

---
+++ README.md
@@ -0,0 +1,18 @@
+# password-manager-server
+
+## Project setup
+```
+npm install
+```
+
+### Environment Variables to run localy
+```
+PORT=3000
+JWT_KEY
+EMAIL_USER
+EMAIL_PASS
+PROTOCOL=http
+MONGODB_USERNAME
+MONGODB_PASSWORD
+MONGO_CONNECTION_STING
+```
--- src/startup/db.js
+++ src/startup/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 module.exports = function () {
-    mongoose.connect('mongodb+srv://admin:' + process.env.MONGO_ATLAS_PW +'@accounts-manager-s9yd9.mongodb.net/test?retryWrites=true&w=majority', {
+    mongoose.connect('mongodb+srv://' + process.env.MONGODB_USERNAME + ':' + process.env.MONGODB_PASSWORD + process.env.MONGO_CONNECTION_STING, {
     useUnifiedTopology: true,
     useNewUrlParser: true
-})
+    })
 }
